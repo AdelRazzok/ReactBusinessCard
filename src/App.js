eef92209d02e359c33556282eef47f61334568a6
= <LHP6.arazzok@gmail.com>
2022-03-04 16:20:39 +0100

    add infos component and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Infos from './components/Infos'
 
 export default function App() {
 	return (
 		<div className="App">
-			
+			<Infos />
 		</div>
-	);
+	)
 }
--- src/components/Infos.js
+++ src/components/Infos.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import infosPicture from '../img/infos-picture.png'
 
 export default function Infos() {
   return (
-	<div>Infos</div>
+	<div className='Infos'>
+		<img
+			src={infosPicture}
+			alt='Picture of the card owner'
+			className='infos-picture'
+		/>
+		<h1 className='infos-title title'>Laura Smith</h1>
+		<p className='infos-desc'>Frontend Developer</p>
+		<p className="infos-website">laurasmith.website</p>
+		<button
+			type='button'
+			className='infos-btn'
+		>
+			<i className='bx bxs-envelope'></i> Email
+		</button>
+	</div>
   )
 }
