a7cb7a97ec15069b015900d703385bd3def5d0fd
= <LHP6.arazzok@gmail.com>
2022-03-05 17:55:34 +0100

    add title and text colors

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Interests() {
   return (
 	<div className='Interests'>
-		<h2 className="interests-title">Interests</h2>
-		<p className="interests-desc">
+		<h2 className="interests-title title">Interests</h2>
+		<p className="interests-desc text">
 			Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
 		</p>
 	</div>
